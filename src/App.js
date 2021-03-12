b78b8413b00cb49061f4ae46f8e0dda411d8d4c1
Ryan Beausoleil <ryanbeausoleil@gmail.com>
2021-03-12 17:11:13 -0500

    Add Projects and update styling

--- src/App.js
+++ src/App.js
@@ -54,39 +54,39 @@ function App() {
       <div className="content-container">
         <Grid container spacing={2}
           justify="center">
-          <Grid item lg={6}>
-            <Fade delay={100} duration={400}>
-              <p className="text">Experienced back-end developer in RESTful architecture, GraphQL or gRPC protocols. Driven to follow standards such as structured JSON logging useful for debugging and data analytics querying, and most importantly security and monitoring.<br /><i class="far fa-circle"></i><br />Developed an efficient rules service using Open Policy Agent for nationwide broker quoting.</p>
-            </Fade>
-          </Grid>
           <Grid item xl={4} className="dev-icons">
-            <Bounce right delay={100}>
-              <h3 className="title">Technical Skills</h3>
+            <Bounce left>
+              <h3 className="title"><i class="fas fa-book"></i> Technical Skills</h3>
             </Bounce>
             <Flip bottom delay={300}>
               <div>
-                <i className="devicon-go-plain" title="Go"></i>
-                <i className="devicon-javascript-plain" title="JavaScript"></i>
-                <i className="devicon-react-original" title="React"></i>
-                <i className="devicon-git-plain" title="Git"></i>
+                <i className="devicon devicon-go-plain" title="Go"></i>
+                <i className="devicon devicon-javascript-plain" title="JavaScript"></i>
+                <i className="devicon devicon-react-original" title="React"></i>
+                <i className="devicon devicon-git-plain" title="Git"></i>
               </div>
             </Flip>
             <Flip bottom delay={600}>
               <div>
-                <i className="devicon-csharp-plain" title="C#"></i>
-                <i className="devicon-nodejs-plain" title="Nodejs"></i>
-                <i className="devicon-mysql-plain" title="MySQL"></i>
-                <i className="devicon-docker-plain" title="Docker"></i>
+                <i className="devicon devicon-csharp-plain" title="C#"></i>
+                <i className="devicon devicon-nodejs-plain" title="Nodejs"></i>
+                <i className="devicon devicon-mysql-plain" title="MySQL"></i>
+                <i className="devicon devicon-docker-plain" title="Docker"></i>
               </div>
             </Flip>
             <Flip bottom delay={900}>
               <div>
-                <i className="devicon-cplusplus-plain" title="C++"></i>
-                <i className="devicon-java-plain" title="Java"></i>
-                <i className="devicon-oracle-original" title="Oracle"></i>
-                <i className="devicon-kubernetes-plain" title="Kubernetes"></i>
+                <i className="devicon devicon-cplusplus-plain" title="C++"></i>
+                <i className="devicon devicon-java-plain" title="Java"></i>
+                <i className="devicon devicon-oracle-original" title="Oracle"></i>
+                <i className="devicon devicon-kubernetes-plain" title="Kubernetes"></i>
               </div>
             </Flip>
+          </Grid>
+          <Grid item lg={6}>
+            <Fade delay={800}>
+              <p className="text">Experienced back-end developer in RESTful architecture, GraphQL or gRPC protocols. Driven to follow standards such as structured JSON logging useful for debugging and data analytics querying, and most importantly security and monitoring.<br /><i class="far fa-circle"></i><br />Developed an efficient rules service using Open Policy Agent for nationwide broker quoting.</p>
+            </Fade>
           </Grid>
         </Grid>
       </div>
@@ -188,6 +188,129 @@ function App() {
                 <li>Handled management of volatile network uncertainties within the automated scanning process</li>
                 <li>Regularly demonstrating software and code to client, and delivering on expectations, while designing and planning for future capabilities</li>
               </ul>
+            </Fade>
+          </Grid>
+        </Grid>
+
+      </div>
+        
+      <hr />
+
+      <div className="content-container">
+
+        <Fade top delay={100}>
+          <h3 className="title"><i class="fas fa-briefcase"></i> Projects</h3>
+        </Fade>
+
+        <Grid container spacing={2}
+          justify="center"
+          alignItems="center"
+          className="project-grid">
+          <Grid item xl={4}>
+            <Fade left>
+              <div className="iframe-centered">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Hooshu3dcqs?version=3&vq=hd720" frameborder="0" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                  allowfullscreen="allowfullscreen"
+                  mozallowfullscreen="mozallowfullscreen" 
+                  msallowfullscreen="msallowfullscreen" 
+                  oallowfullscreen="oallowfullscreen" 
+                  webkitallowfullscreen="webkitallowfullscreen"></iframe>
+              </div>
+            </Fade>
+          </Grid>
+          <Grid item xl={6}>
+            <Fade right>
+              <h4 className="project-title">Firestorm</h4>
+            </Fade>
+            <Fade right delay={300}>
+              <p className="project-text">
+                Developed a professional 3D game in the span of 3 months, using C# MonoGame for the course <b>Game Programming with Data Structures</b>. Designed with custom 2D collision detections, billboard particle emissions, frustum model culling, and for efficient garbage and resource management.
+              </p>
+            </Fade>
+            <Fade right delay={500}>
+              <div><a href="https://github.com/ryanbeau/firestorm">View on Github</a></div>
+            </Fade>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2}
+          justify="center"
+          alignItems="center"
+          className="project-grid">
+          <Grid item xl={6}>
+            <Fade left>
+              <h4 className="project-title">USA Covid &#38; Political Map</h4>
+            </Fade>
+            <Fade left delay={300}>
+              <p className="project-text">
+                Developed an ASP .NET Core web API to project the 2016 USA political information with an overlay of COVID-19 data to compare democrat vs republican rates of infection. Using the Mapbox API and the COVID19 data is requested from the arcgis.com API. Basic JWT Authentication.
+              </p>
+            </Fade>
+            <Fade left delay={500}>
+              <div><a href="https://github.com/ryanbeau/usa-covid-political">View on Github</a></div>
+            </Fade>
+          </Grid>
+          <Grid item xl={4}>
+            <Fade right>
+              <img src="https://github.com/ryanbeau/usa-covid-political/raw/main/screenshot.png" 
+                title="USA Covid Political Map" 
+                alt="Screenshot of USA Covid Political Map" 
+                className="project-image" />
+            </Fade>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2}
+          justify="center"
+          alignItems="center"
+          className="project-grid">
+          <Grid item xl={4}>
+            <Fade left>
+              <img src="https://github.com/ryanbeau/games-store/raw/master/games-screenshot.png" 
+                title="Game Store" 
+                alt="Screenshot of the Game Store web application" 
+                className="project-image" />
+            </Fade>
+          </Grid>
+          <Grid item xl={6}>
+            <Fade right>
+              <h4 className="project-title">Game Store</h4>
+            </Fade>
+            <Fade right delay={300}>
+              <p className="project-text">
+                Group project developed with 4 students, an ASP .NET Core MVC web eCommerce games store. Using entity framework for DB access. Developed with games discounts, custom sorting, wishlisting, add/remove to cart features, and checkout.
+              </p>
+            </Fade>
+            <Fade right delay={500}>
+              <div><a href="https://github.com/ryanbeau/games-store">View on Github</a> • <a href="https://moistgaming.azurewebsites.net/">Visit Site</a></div>
+            </Fade>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2}
+          justify="center"
+          alignItems="center"
+          className="project-grid">
+          <Grid item xl={6}>
+            <Fade left>
+              <h4 className="project-title">RC Controller</h4>
+            </Fade>
+            <Fade left delay={300}>
+              <p className="project-text">
+                Personal hobby project using ESP32 embedded systems Arduino programming environment in C++. Constrained to the size limitations of the 8-MB of flash memory, the 512-byte EEPROM, and the 2.4GHz transceiver's 32-byte packet size.
+              </p>
+            </Fade>
+            <Fade left delay={500}>
+              <div><a href="https://github.com/ryanbeau/rc-remote">View on Github</a></div>
+            </Fade>
+          </Grid>
+          <Grid item xl={4}>
+            <Fade right>
+              <img src="https://github.com/ryanbeau/rc-remote/raw/main/controller.jpg" 
+                title="RC Remote Controller" 
+                alt="RC Remote Controller with LCD touch screen" 
+                className="project-image" />
             </Fade>
           </Grid>
         </Grid>
